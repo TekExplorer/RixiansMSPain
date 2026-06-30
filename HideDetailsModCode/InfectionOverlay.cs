14604a260206a013abe07ffcff8f762873e3e351
Omar Kamel <tekexplorerm@gmail.com>
2026-06-30 16:29:39 -0400

    shift overlay images to a more reasonable location

---
+++ HideDetailsModCode/InfectionOverlay.cs
@@ -0,0 +1,20 @@
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+
+namespace HideDetailsMod.HideDetailsModCode;
+
+[HarmonyPatch]
+class InfectionOverlay
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CardModel), nameof(CardModel.OverlayPath), MethodType.Getter)]
+    static void SwitchToRed(CardModel __instance, ref string __result)
+    {
+        if (__instance is Infection)
+            __result = SceneHelper.GetScenePath("cards/overlays/" + "red_" + __instance.Id.Entry.ToLowerInvariant());
+    }
+
+    // const string path = "res://HideDetailsMod/scenes/overlays/red_infection.tscn";
+}
