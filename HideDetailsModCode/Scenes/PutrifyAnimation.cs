c245a1b6c89730ec2cab967e0282efd71486ca29
Omar Kamel <tekexplorerm@gmail.com>
2026-07-23 14:51:39 -0400

    Update credits a bit more

---
+++ HideDetailsModCode/Scenes/PutrifyAnimation.cs
@@ -0,0 +1,23 @@
+// using BaseLib.Utils;
+// using Godot;
+// using MegaCrit.Sts2.Core.Models.Cards;
+// using MegaCrit.Sts2.Core.Nodes.Cards;
+
+// namespace HideDetailsMod.HideDetailsModCode.Scenes;
+
+// partial class PutrifyAnimated : Control
+// {
+//     static internal AddedNode<NCard, BundleOfJoyAnimated> _ = new("res://HideDetailsMod/scenes/cards/putrify.tscn", (card, animation) => animation.card = card);
+//     // res://HideDetailsMod/scenes/cards/bundle_of_joy.tscn
+//     NCard? card;
+//     AnimatedSprite2D? animation;
+//     public override void _Ready()
+//     {
+//         animation = GetNodeOrNull<AnimatedSprite2D>("AnimatedSprite2D");
+//     }
+
+//     public override void _Process(double delta)
+//     {
+//         Visible = card?.Model is BundleOfJoy;
+//     }
+// }
