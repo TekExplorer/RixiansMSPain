50d8fe38a44fdf044e1261773a165c68fab17372
Omar Kamel <tekexplorerm@gmail.com>
2026-08-30 15:52:30 -0400

    Dramatically improve necrobinder background - Trimmed images to make them easier to work with in some ways - Osty moves with the info box - Osty and Necro fire undulates - Background image also undulates - Scythe glints

--- HideDetailsModCode/Scenes/CharacterSelect/NecrobinderBg.cs
+++ HideDetailsModCode/Scenes/CharacterSelect/NecrobinderBg.cs
@@ -1,5 +1,6 @@
 using Godot;
 using HarmonyLib;
+using MegaCrit.Sts2.addons.mega_text;
 using MegaCrit.Sts2.Core.Nodes.GodotExtensions;
 using MegaCrit.Sts2.Core.Nodes.Screens.CharacterSelect;
 using System;
@@ -18,23 +19,28 @@ static public class BeginRunPatch
 
 public partial class NecrobinderBg : Control
 {
-	// NCharacterSelectScreen? nCharacterSelectScreen;
+#nullable disable
+	public TextureRect OstyFingers;
+	public Control Osty; // Keep as Control to match the container type in your scene file
+#nullable restore
+	NCharacterSelectScreen? nCharacterSelectScreen;
+	Control? InfoPanel => nCharacterSelectScreen?._infoPanel;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// nCharacterSelectScreen = this.GetAncestorOfType<NCharacterSelectScreen>();
-		// nCharacterSelectScreen.OnEmbarkPressed;
-		// nCharacterSelectScreen.Lobby._isBeginningRun;
-		OstyFingers = GetNode<TextureRect>("%OstyFingers");
+		nCharacterSelectScreen = this.GetAncestorOfType<NCharacterSelectScreen>();
+		Osty = GetNode<Control>("%Osty"); // Corrected type to match the scene node layout
+		OstyFingers = GetNode<TextureRect>("%OstyFront");
 		OstyFingers.ZIndex = 1;
 	}
-	public TextureRect OstyFingers;
+
 	internal void ResetFingersPosition()
 	{
 		OstyFingers.ZIndex = 0;
 	}
 
-	static public NecrobinderBg? Node;
+	static public NecrobinderBg? Node { get; set; }
 
 	public override void _EnterTree()
 	{
@@ -49,5 +55,19 @@ public partial class NecrobinderBg : Control
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (IsInstanceValid(InfoPanel))
+		{
+			// 1. Calculate the true center of the target InfoPanel in screen space
+			Vector2 targetCenterGlobal = InfoPanel.GlobalPosition + (InfoPanel.Size * 0.5f);
+
+			// 2. Convert that screen center point into local coordinates for this Control
+			Vector2 localTargetCenter = GetGlobalTransform().AffineInverse() * targetCenterGlobal;
+
+			// 3. Offset by Osty's own center size (scaled properly) so it is perfectly centered
+			Vector2 ostyCenterOffset = Osty.Size * 0.5f * Osty.Scale;
+
+			// 4. Move Osty to the target center, subtracting its own center point
+			Osty.Position = localTargetCenter - ostyCenterOffset;
+		}
 	}
 }
